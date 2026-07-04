bc6407204d56c17b4f1d27a842746e3067cef9e8
Swastik Patel <dev.swastikpatel0305@gmail.com>
2026-07-04 20:41:08 +0530

    comit

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/flip/',
+    base: '/flipco/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
